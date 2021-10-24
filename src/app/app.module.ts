ee86c9dabfa7c18b8e012ab3a2fcdd9c4e16a294
Shailesh <shaileshrai1001@gmail.com>
2021-10-24 22:03:23 +0530

    user & admin components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routeApp:Routes=[
+  {path:'user',component:UserComponent},
+  {path:'admin',component:AdminComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    AdminComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeApp)
   ],
   providers: [],
   bootstrap: [AppComponent]
